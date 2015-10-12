a94e97e3a7b0b405e8906d554b73bd26f5e13382
Karl Horky <karl.horky@gmail.com>
2015-10-12 12:28:42 +0200

    Gzip to directory

--- gzip-web-assets.sh
+++ gzip-web-assets.sh
@@ -2,6 +2,8 @@
 
 # Usage:
 # $ ./gzip-web-assets.sh
+
+mkdir gzipped
 for file in $(find . -type f -depth 1 | egrep "\.(css|js|eot|svg|ttf)$") ; do
-  gzip -c --best $file > $file.gz
+  gzip -c --best $file > gzipped/$file
 done
