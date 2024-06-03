d85e6a00b086da84e1b303edd710c172915c857c
Karl Horky <karl.horky@gmail.com>
2024-06-03 14:58:17 +0200

    Delete gzip-web-assets.sh

--- gzip-web-assets.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Usage:
-# $ ./gzip-web-assets.sh
-
-mkdir gzipped
-for file in $(find . -type f -depth 1 | egrep "\.(css|js|eot|svg|ttf)$") ; do
-  gzip -c --best $file > gzipped/$file
-done
